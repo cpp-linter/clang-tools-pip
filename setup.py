ea30ba74f5dddbbcf4b580d2da3b8b1ba20fa4be
Brendan <2bndy5@gmail.com>
2022-08-07 02:16:08 -0700

    update workflows and build binary wheel
    Switch installation config to pyproject.toml. The setup.py file remains as a dummy until it's usage is completely deprecated.
    
    Binary wheels uploaded to pypi will download and install quicker than software distributions (tar.gz archives).
    
    Build docs in CI, but only publish to gh-pages from main (on push events). Save built docs as artifact.
    
    Install job uses built wheel from build job (which uploads a binary wheel as artifact)
    
    Use official codecov action to upload reports (doesn't require a token)
    
    Publishing workflow only uploads to PyPI on a published release event. If it is invoked by workflow_dispatch event, then uploads to testPyPI.

--- setup.py
+++ setup.py
@@ -1,43 +1,11 @@
-"""Info used for installing the clang-tools package."""
-from setuptools import find_packages
-from setuptools import setup
+#!/usr/bin/env python
+"""Info used for installing the clang-tools package.
 
-with open("README.md", "r", encoding='utf-8') as file:
-    long_description = file.read()
+Since using setup.py is no longer std convention,
+all install information is located in pyproject.toml
+"""
+
+import setuptools
 
 
-setup(
-    name="clang_tools",
-    description="Install clang-tools (clang-format, clang-tidy) with pip",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Peter Shen",
-    author_email="xianpeng.shen@gmail.com",
-    keywords=["clang", "clang-tools", "clang-extra", "clang-tidy", "clang-format"],
-    license="MIT License",
-    use_scm_version={"local_scheme": "no-local-version"},
-    setup_requires=['setuptools_scm'],
-    packages=find_packages(),
-    project_urls={
-        "Source": "https://github.com/cpp-linter/clang-tools-pip",
-        "Tracker": "https://github.com/cpp-linter/clang-tools-pip/issues"
-    },
-    classifiers=[
-        # https://pypi.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: Information Technology",
-        "Natural Language :: English",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Build Tools",
-    ],
-    entry_points={
-        "console_scripts": [
-            "clang-tools=clang_tools.main:main"
-        ]
-    },
-)
+setuptools.setup()

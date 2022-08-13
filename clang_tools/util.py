5955d41c2712dd2599c61580c1d10c51a59b1626
shenxianpeng <xianpeng.shen@gmail.com>
2022-08-13 02:52:08 +0000

    Change download status

--- clang_tools/util.py
+++ clang_tools/util.py
@@ -52,13 +52,13 @@ def download_file(url: str, file_name: str) -> Optional[str]:
         block_size = int(length / 20)
         # show completed
         completed = len(buffer) / length
-        print("    |" + "-" * int(completed * 20), end="")
+        print("    |" + "█" * int(completed * 20), end="")
         print(" " * math.ceil((1 - completed) * 20), end="|")
         print(f"{int(completed * 100)}% (of {length} bytes)", end="\r")
         remaining = length - len(buffer)
         buffer += response.read(block_size if remaining > block_size else remaining)
     response.close()
-    print("    |" + ("-" * 20) + f"|100% (of {length} bytes)")
+    print("    |" + ("█" * 20) + f"| 100% (of {length} bytes)")
     file = Path(file_name)
     file.write_bytes(buffer)
     return file.as_posix()
